e0375b595f3300a15302dd1b698aa3b7cbc1a853
abhijeet <abhijeet.t91@gmail.com>
2017-01-18 01:29:33 -0500

    moved some functions

--- mat/examples/getNPtsPerCluster.m
+++
@@ -1,9 +0,0 @@
-function nPtsPerCluster = getNPtsPerCluster(clusterIds)
-nClusters = length(unique(clusterIds));
-nPts = length(clusterIds);
-clusterIds = flipVecToColumn(clusterIds);
-clusterIdsMat = repmat(clusterIds,1,nClusters);
-matchMat = repmat(1:nClusters,nPts,1);
-flagMat = (clusterIdsMat == matchMat);
-nPtsPerCluster = sum(flagMat,1);
-end
