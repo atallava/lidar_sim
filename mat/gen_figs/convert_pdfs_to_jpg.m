20752f7121fa22bf6b6c26978f9113862db3aa2f
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2017-03-02 15:37:02 -0500

    mat. convert pdfs to jpgs.

---
+++ mat/gen_figs/convert_pdfs_to_jpg.m
@@ -0,0 +1,13 @@
+genRelPathPdf = @(fname) sprintf('~/thesis_proposal/tex/figs/offroad_lidar_sim/%s.pdf',fname);
+genRelPathJpg = @(fname) sprintf('~/thesis_proposal/tex/figs/jpgs/%s.jpg',fname);
+
+fnames = {'algo_dev_path','algo_dev_real','algo_dev_sim_1','algo_risk_sim_1','algo_risk_sim_2','algo_risk_sim_2_extra', ...
+    'classes_in_frame','ellipsoid_intersections','ellipsoid_models','flowchart_sim','flowchart_sim_color','flowchart_train',...
+    'flowchart_train_color','gantt','map','map_unannotated','map_with_pcd','primitives','scene_sim','sim_star_objective_gap','single_scene_sim',...
+    'sparser_state_mapping','triangle_mesh_model','voxnet_data_cartoon'};
+
+for i = 1:length(fnames)
+    fname = fnames{i};
+    cmd = sprintf('convert -density 300 -quality 100 %s %s',genRelPathPdf(fname),genRelPathJpg(fname));
+    system(cmd);
+end
