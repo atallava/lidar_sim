135cb28db600d43a1e664e8fb6c4568e49e84c38
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2017-04-21 00:14:36 -0400

    cpp. created scene_sim/

--- cpp/hybrid_geometric_sim/section_slice_hg_sim.cpp
+++ cpp/hybrid_geometric_sim/section_slice_hg_sim.cpp
@@ -150,7 +150,7 @@ int main(int argc, char **argv)
     std::vector<int> ellipsoid_blocks_queried;
     std::vector<int> triangle_blocks_queried;
     std::vector<std::vector<double> > sim_detail;
-    size_t packet_array_step = 10; // 100
+    size_t packet_array_step = 100; // 100
     for(size_t i = packet_id_sim_start; 
 	i < packet_id_sim_end; i += packet_array_step)
     {
