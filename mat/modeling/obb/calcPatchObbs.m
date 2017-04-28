b655624b22672e66366ce831acd99854a2c3bab8
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2017-04-28 00:25:39 -0400

    mat. patch obbs.

---
+++ mat/modeling/obb/calcPatchObbs.m
@@ -0,0 +1,89 @@
+function [obbCell,obbPtsCell] = calcPatchObbs(pts)
+    %CALCPATCHOBBS
+    %
+    % [obbCell,obbPtsCell] = CALCPATCHOBBS(pts)
+    %
+    % pts        -
+    %
+    % obbCell    -
+    % obbPtsCell -
+    
+    % xy-frame
+    ptsXy = pts(:,1:2);
+    frameCenter = mean(ptsXy,1);
+    ptsXyCentered = bsxfun(@minus,ptsXy,frameCenter);
+   
+    frameExtents = zeros(2,2);
+    lowLim = 1e-2;
+    highLim = 1-lowLim;
+    for i = 1:2
+        frameExtents(i,1) = quantile(ptsXyCentered(:,i),lowLim);
+        frameExtents(i,2) = quantile(ptsXyCentered(:,i),highLim);
+    end
+   
+    % grid
+    nodeResn = 4;
+    numXNodes = ceil((frameExtents(1,2)-frameExtents(1,1))/nodeResn);
+    numYNodes = ceil((frameExtents(2,2)-frameExtents(2,1))/nodeResn);
+    xNodes = [0:(numXNodes-1)]*nodeResn + 0.5*nodeResn + frameCenter(1)+frameExtents(1,1);
+    yNodes = [0:(numYNodes-1)]*nodeResn + 0.5*nodeResn + frameCenter(2)+frameExtents(2,1);
+    [xGridMat,yGridMat] = meshgrid(xNodes,yNodes);
+    xGrid = xGridMat(:);
+    yGrid = yGridMat(:);
+    
+    % decide which to keep 
+    gridOccupancyCount = zeros(numYNodes,numXNodes);
+    gridOccupancyPtIds = cell(numYNodes,numXNodes);
+    for i = 1:size(pts,1)
+        pt = ptsXy(i,:);
+        if checkInFrame(pt)
+            idX = ceil((pt(1)-(frameCenter(1)+frameExtents(1,1)))/nodeResn);
+            idY = ceil((pt(2)-(frameCenter(2)+frameExtents(2,1)))/nodeResn); 
+            gridOccupancyCount(idY,idX) = ...
+                gridOccupancyCount(idY,idX)+1;
+            gridOccupancyPtIds{idY,idX} = ...
+                [gridOccupancyPtIds{idY,idX} i];
+        end
+    end
+    minOccupancy = 10;
+    occupancyFlagMat = gridOccupancyCount >= minOccupancy;
+    occupancyFlag = occupancyFlagMat(:);
+    
+    % for each, z-extents
+    gridIds = find(occupancyFlag);
+    nObbs = sum(occupancyFlag);
+    obbCell = cell(1,nObbs);
+    obbPtsCell = cell(1,nObbs);
+    for i = 1:nObbs
+        gridId = gridIds(i);
+        [gridRow,gridCol] = ind2sub(size(gridOccupancyPtIds),gridId);
+        ptsInGridIds = gridOccupancyPtIds{gridRow,gridCol};
+        ptsInGrid = pts(ptsInGridIds,:);
+        zVec = ptsInGrid(:,3);
+        zVecCentered = zVec-mean(zVec);
+        obb.center = [xGrid(gridId) yGrid(gridId) mean(zVec)];
+        
+        
+        obb.ax1 = [1 0];
+        obb.ax2 = [0 1];
+        
+        obb.extents(1,1) = -0.5*nodeResn;
+        obb.extents(1,2) = 0.5*nodeResn;
+        obb.extents(2,1) = -0.5*nodeResn;
+        obb.extents(2,2) = 0.5*nodeResn;
+        obb.extents(3,1) = quantile(zVecCentered,lowLim);
+        obb.extents(3,2) = quantile(zVecCentered,highLim);
+        
+        obbCell{i} = obb;
+        obbPtsCell{i} = ptsInGrid;
+    end
+
+    %% helpers
+    function res = checkInFrame(pt)
+        condn1 = frameCenter(1)+frameExtents(1,1) <= pt(1);
+        condn2 = pt(1) <= frameCenter(1)+frameExtents(1,2);
+        condn3 = frameCenter(2)+frameExtents(2,1) <= pt(2);
+        condn4 = pt(2) <= frameCenter(2)+frameExtents(2,2);
+        res = condn1 & condn2 & condn3 & condn4;
+    end
+end
---
+++ mat/scene_sim/annotation_patch.m
@@ -0,0 +1,55 @@
+% rel path helpers
+genRelPathPtsMat = @(sectionId,segmentId) ...
+    sprintf('../data/sections/section_%02d/non_ground_segmentation/%d.mat', ...
+    sectionId,segmentId);
+
+genRelPathEllipsoids = @(sectionId,blockId) ...
+    sprintf('../data/sections/section_%02d/section_%02d_block_%02d_non_ground_ellipsoids.mat', ...
+    sectionId,sectionId,blockId);
+
+%% load ellipsoids
+sectionId = 3;
+relPathDir = sprintf('../data/sections/section_%02d',sectionId);
+pattern = sprintf('section_%02d_block_([0-9]+)_non_ground_ellipsoids.mat', ...
+    sectionId);
+[matchingFiles,blockIds] = getPatternMatchingFileIds(relPathDir,pattern);
+ellipsoidModelsCell = cell(1,length(blockIds));
+for i = 1:length(blockIds)
+    relPathEllipsoids = genRelPathEllipsoids(sectionId,blockIds(i));
+    can = load(relPathEllipsoids,'ellipsoidModels');
+    ellipsoidModelsCell{i} = can.ellipsoidModels;
+end
+ellipsoidModels = stitchEllipsoidModels(ellipsoidModelsCell);
+
+%% load pts
+sectionId = 3;
+segmentId = 112;
+relPathPtsMat = genRelPathPtsMat(sectionId,segmentId);
+load(relPathPtsMat,'pts');
+
+%% patch obbs
+[patchObbs,ptsInObbs] = calcPatchObbs(pts);
+% ellipsoids for each
+patchEllipsoids = cell(size(patchObbs));
+for i = 1:length(patchObbs)
+    patchEllipsoids{i} = calcEllipsoidsInObb( ...
+        ellipsoidModels,patchObbs{i});
+end
+patchEllipsoidsAll = stitchEllipsoidModels(patchEllipsoids);
+
+%% viz ellipsoids
+plotStructVars = {'ellipsoidData','plotStruct'};
+clear(plotStructVars{:});
+% ellipsoidData.ellipsoidModels = patchEllipsoidsAll;
+ellipsoidData.ellipsoidModels = patchEllipsoids{1};
+ellipsoidData.uniformAlpha = false;
+plotStruct.ellipsoidData = ellipsoidData;
+hfig = plotRangeData(plotStruct);
+
+%% viz obbs
+scatter3(pts(:,1),pts(:,2),pts(:,3),'r.');
+for i = 1:length(patchObbs)
+    drawObb(hfig,patchObbs{i});
+end
+
+
