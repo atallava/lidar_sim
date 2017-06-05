d8229b9c27a42c3bb816ff2180f2f342984a1a3f
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2017-06-04 23:50:35 -0400

    cpp. mesh model sim, needs debug.

--- cpp/include/lidar_sim/MeshModelSim.h
+++ cpp/include/lidar_sim/MeshModelSim.h
@@ -17,21 +17,24 @@ namespace lidar_sim {
 	void calcObjectCentroids();
 	std::vector<int> calcObjectIdsForSim(const std::vector<double> &ray_origin, 
 					     const std::vector<double> &ray_dirn);
-	/* void loadTriangleModelBlocks(const std::vector<std::string> &rel_path_model_blocks); */
-	/* void loadBlockInfo(const std::string rel_path_imu_posn_nodes, const std::string rel_path_block_node_ids_ground, const std::string rel_path_block_node_ids_non_ground); */
+	std::vector<int> calcObjectIdsForSim(const std::vector<double> &ray_origin, 
+					     const std::vector<std::vector<double> > &ray_dirns);
+	void loadTriangleModelBlocks(const std::vector<std::string> &rel_path_model_blocks);
+	void loadBlockInfo(const std::string rel_path_imu_posn_nodes, 
+			   const std::string rel_path_block_node_ids_ground);
 
-	/* std::vector<int> getPosnTriangleBlockMembership(const std::vector<double> &imu_pose); */
-	/* std::vector<int> getPoseBlockMembership(const std::vector<double> &imu_pose, const std::vector<std::vector<int> > &block_node_ids); */
-	/* std::vector<int> getPosnBlockMembership(const std::vector<double> &posn, const std::vector<std::vector<int> > &block_node_ids); */
+	std::vector<int> getPosnTriangleBlockMembership(const std::vector<double> &imu_pose);
+	std::vector<int> getPoseBlockMembership(const std::vector<double> &imu_pose, const std::vector<std::vector<int> > &block_node_ids);
+	std::vector<int> getPosnBlockMembership(const std::vector<double> &posn, const std::vector<std::vector<int> > &block_node_ids);
 	
-	/* std::tuple<std::vector<std::vector<double> >, std::vector<int> > */
-	/*     simPtsGivenPose(const std::vector<double> &imu_pose); */
-	/* std::tuple<std::vector<std::vector<double> >, std::vector<int> >  */
-	/*     simPtsGivenPoses(const std::vector<std::vector<double> > &imu_poses); */
-	/* std::tuple<std::vector<std::vector<double> >, std::vector<int> >  */
-	/*     simPtsGivenRays(const std::vector<double> &ray_origin, const std::vector<std::vector<double> > &ray_dirns); */
+	std::tuple<std::vector<std::vector<double> >, std::vector<int> >
+	    simPtsGivenPose(const std::vector<double> &imu_pose);
+	std::tuple<std::vector<std::vector<double> >, std::vector<int> >
+	    simPtsGivenPoses(const std::vector<std::vector<double> > &imu_poses);
+	std::tuple<std::vector<std::vector<double> >, std::vector<int> >
+	    simPtsGivenRays(const std::vector<double> &ray_origin, const std::vector<std::vector<double> > &ray_dirns);
 
-	/* void setDeterministicSim(const bool choice); */
+	void setDeterministicSim(const bool choice);
 
 	std::vector<TriangleModelSim> m_object_mesh_sims;
 	std::vector<std::vector<double> > m_object_centroids;
