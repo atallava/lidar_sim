06ef494ea8fd7feec9bf861d3cfbed65affa3c1a
abhijeet <abhijeet.t91@gmail.com>
2017-06-09 14:19:09 -0400

    mat. gen roster meshes.

---
+++ mat/gen_figs/gen_meshes_for_roster.m
@@ -0,0 +1,34 @@
+% mesh primitives
+genRelPathMeshPrimitive = @(className,elementId) ...
+    sprintf('../data/3d_models/primitives/%s/%d',className,elementId);
+
+relPathPrimitiveClasses = '../data/primitive_classes';
+load(relPathPrimitiveClasses,'primitiveClasses','primitiveClassIsPatch');
+
+% add export_fig to path
+someUsefulPaths;
+addpath([pathToM '/altmany-export_fig-5be2ca4']);
+
+%%
+classId = 5;
+% elementIdsPerClass = {[1,3],[],[],[],[7,10,11,16],[],[],[],[1,2,3,4],[],[2,6,15]} 
+elementId  = 5;
+
+className = primitiveClasses{classId};
+relPathMeshPrimitive = genRelPathMeshPrimitive(className,elementId);
+load(relPathMeshPrimitive,'triModels');
+
+% plot
+hfig = vizTriModels(triModels);
+viewAngles = ([0 0]);
+view(viewAngles);
+% for consistency
+zLim = zlim;
+zLimRange = 16;
+zLim = [zLim(1) zLim(1)+zLimRange];
+zlim(zLim);
+set(gca,'visible','off');
+
+% relPathPng = sprintf('elements_pngs/%s_%d.png',className,elementId);
+% export_fig(relPathPng,hfig);
+
