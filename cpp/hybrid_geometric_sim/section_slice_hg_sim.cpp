a66290ffdb57db3c4b26fdfa5e14274761b7ad77
Abhijeet Tallavajhula <abhijeet.t91@gmail.com>
2017-07-06 14:37:56 -0400

    cpp. ray nn and old match for radius 5.

--- cpp/hybrid_geometric_sim/section_slice_hg_sim.cpp
+++ cpp/hybrid_geometric_sim/section_slice_hg_sim.cpp
@@ -169,7 +169,7 @@ int main(int argc, char **argv)
 
     // slice ids
     size_t packet_id_sim_start, packet_id_sim_end;
-    packet_id_sim_start = 1000; // 40000 // 50000
+    packet_id_sim_start = 50000; // 40000 // 50000
     packet_id_sim_end = packet_id_sim_start + 10000; // 20000
 
     // sim
@@ -180,7 +180,7 @@ int main(int argc, char **argv)
     std::vector<int> ellipsoid_blocks_queried;
     std::vector<int> triangle_blocks_queried;
     SimDetail sim_detail;
-    size_t packet_array_step = 1000; // 100
+    size_t packet_array_step = 10; // 100
     for(size_t i = packet_id_sim_start; 
 	i < packet_id_sim_end; i += packet_array_step)
     {
