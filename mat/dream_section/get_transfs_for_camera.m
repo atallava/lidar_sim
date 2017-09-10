cf0ec44db80d4d15cb8e2d6d642fb28acee5b10d
abhijeet <abhijeet.t91@gmail.com>
2017-09-10 01:20:41 -0400

    mat. camera transfs for dream section.

---
+++ mat/dream_section/get_transfs_for_camera.m
@@ -0,0 +1,47 @@
+%% rvctoolbox
+someUsefulPaths;
+relPathRvcStartup = [pathToM '/rvctools/startup_rvc.m'];
+run(relPathRvcStartup);
+
+%%
+v = 0.5;
+duration = 180/v;
+
+tStart = 0;
+tEnd = duration;
+
+tResn = 0.1; 
+tKeypoints = tStart:tResn:tEnd;
+nKeypoints = length(tKeypoints);
+
+%% calculate camera params
+cameraPositions = zeros(nKeypoints,3);
+cameraTargets = zeros(nKeypoints,3);
+
+for i = 1:length(tKeypoints)
+t = tKeypoints(i);
+
+x = 0; y = v*t; z = 1;
+cameraPosition = [x y z];
+cameraTarget = [x y+0.1 z];
+
+cameraPositions(i,:) = cameraPosition;
+cameraTargets(i,:) = cameraTarget;
+end
+
+viewDirns = calcViewDirns(cameraPositions,cameraTargets);
+
+%% viz
+% scatter3(pts(:,1),pts(:,2),pts(:,3),'b.');
+axis equal;
+xlabel('x (m)'); ylabel('y (m)'); zlabel('z (m)'); 
+hold on;
+
+quiver3(cameraPositions(:,1),cameraPositions(:,2),cameraPositions(:,3), ...
+    viewDirns(:,1),viewDirns(:,2),viewDirns(:,3),'r');
+
+%% write out
+relPathOutCsv = '../data/rviz/section_42_camera_poses.csv';
+writeCameraTransforms(relPathOutCsv,tKeypoints-tStart,cameraPositions,viewDirns);
+
+ 
